02e3a0384b25fea73d5d9fd3b47fa6c5344cbcc4
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-02-24 08:09:48 +0100

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,30 @@
+
+#include <vector>
+using namespace std;
+
+class Solution {
+    
+public:
+
+    int fixedPoint(vector<int>& arr) {
+
+        int left = 0;
+        int right = arr.size() - 1;
+        int smallestIndex_where_indexEqualsArrayElement = -1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == mid) {
+                smallestIndex_where_indexEqualsArrayElement = mid;
+                right = mid - 1;
+            } else if (arr[mid] < mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return smallestIndex_where_indexEqualsArrayElement;
+    }
+};
